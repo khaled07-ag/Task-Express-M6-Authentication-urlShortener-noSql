14f01b8e92126cefe9ca0838a503bfab73e4d0ec
khaled07-ag <khaled-kk01@hotmail.com>
2024-09-29 20:11:59 +0300

    done

--- api/users/users.controllers.js
+++ api/users/users.controllers.js
@@ -1,9 +1,31 @@
 const User = require("../../models/User");
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+dotenv.config();
+const hashPassword = async(password)=>{
+  const hashedPassword = await bcrypt.hash(password,saltRounds)
+  return hashedPassword;
+};
+const generateToken =  (user)=>{
+  const payload = {
+    id: user._id,
+    username: user.username,
+  }
+  const token = jwt.sign(payload, process.env.JWT_SECRET,{
+    expiresIn:"1h",
+  });
+  return token;
+};
 exports.signup = async (req, res) => {
   try {
+    const {password} = req.body;
+    const newPassword = await hashPassword(password);
+    req.body.password = newPassword;
     const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+    const token = generateToken(newUser);
+    return res.status(201).json({token: token});
   } catch (err) {
     next(err);
   }
@@ -11,6 +33,8 @@ exports.signup = async (req, res) => {
 
 exports.signin = async (req, res) => {
   try {
+    const user = await generateToken(req.user);
+    return res.status(200).json({token: user})
   } catch (err) {
     res.status(500).json("Server Error");
   }
